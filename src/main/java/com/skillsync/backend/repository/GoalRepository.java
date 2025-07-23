79d66a4e102657e6241520eec11a4dd48fd8eb65
annkit007 <singhankit75076@gmail.com>
2025-07-23 17:42:42 +0530

    Removed duplicate backend files from root directory

--- src/main/java/com/skillsync/backend/repository/GoalRepository.java
+++
@@ -1,6 +0,0 @@
-package com.skillsync.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.skillsync.backend.model.Goal;
-
-public interface GoalRepository extends JpaRepository<Goal, Long> {}
